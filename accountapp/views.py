e9c58b6efd85f93324f2145aee609c0eb74dd023
eka8809 <dideka8809@likelion.org>
2021-07-12 10:39:35 +0900

    2021_07_12_commit2

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 from accountapp.models import HelloWorld
-
+from django.urls import reverse
 
 def hello_world(request):
     if request.method == "POST":
@@ -14,9 +14,8 @@ def hello_world(request):
         new_hello_world.text =temp
         new_hello_world.save()
 
-        hello_world_list = HelloWorld.objects.all()
-
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
